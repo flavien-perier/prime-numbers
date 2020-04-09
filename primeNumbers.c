8c5fccfbb39a220cb26ef1439448888e00039f19
Flavien PERIER <flavien.perier@pm.me>
2020-04-09 10:51:16 +0200

    Change thread initialisation

--- primeNumbers.c
+++ primeNumbers.c
@@ -16,23 +16,21 @@
 typedef struct {
 	// Worker context
 	pthread_t thread;
+	sem_t primeNumberBufferedSem;
 
+	unsigned int id;
+	unsigned long long int initValue;
+	unsigned long long int maxValue;
 	unsigned long long int primeNumberBuffer;
 	unsigned char primeNumberBuffered;
 	unsigned char workCompleted;
-	sem_t primeNumberBufferedSem;
 
 	// Global context
 	unsigned long long int *primeList;
 
-	unsigned int *maxRank;
-	unsigned long long int *maxValue;
 	unsigned int *progression;
 	unsigned int *nbrThreads;
-	unsigned long long int *initValue;
 	unsigned char *workInProgress;
-
-	sem_t *workerOrchestrator;
 } Task;
 
 unsigned char isInteger(long double value) {
@@ -45,12 +43,10 @@ unsigned char isInteger(long double value) {
 void *primeNumbersWorker(void *args) {
 	Task *task = (Task*)args;
 
-	sem_wait(task->workerOrchestrator);
-
 	unsigned char isPrime;
 	register unsigned int i;
 
-	for (register unsigned long long int value = *task->initValue; value <= *task->maxValue && *task->workInProgress; value += (2 * *task->nbrThreads)) {
+	for (register unsigned long long int value = task->initValue; value <= task->maxValue && *task->workInProgress; value += (2 * *task->nbrThreads)) {
 		isPrime = 1;
 		for (i = 1; i < *task->progression && task->primeList[i] < value >> 1; i++) {
 			if (isInteger(value * 1.0 / task->primeList[i])) {
@@ -75,7 +71,6 @@ unsigned long long int *primeNumbers(unsigned int *maxRank, unsigned long long i
 
 	unsigned int nbrThreads = 1;
 	unsigned int progression = 2;
-	unsigned long long int initValue = 5;
 	unsigned char workInProgress = 1;
 
 	pthread_mutex_t progressionMutex = PTHREAD_MUTEX_INITIALIZER;
@@ -86,48 +81,46 @@ unsigned long long int *primeNumbers(unsigned int *maxRank, unsigned long long i
 
 	Task **tasks = (Task**)malloc(sizeof(Task) * nbrMaxThreads);
 
-	sem_t workerOrchestrator;
-	sem_init(&workerOrchestrator, 0, 1);
-
 	// Initializes all tasks
 	for (i = 0; i < nbrMaxThreads; i++) {
 		tasks[i] = (Task*)malloc(sizeof(Task));
 
+		sem_init(&tasks[i]->primeNumberBufferedSem, 0, 0);
+
+		tasks[i]->id = i;
+		tasks[i]->maxValue = *maxValue;
 		tasks[i]->primeNumberBuffered = 0;
 		tasks[i]->workCompleted = 0;
-		sem_init(&tasks[i]->primeNumberBufferedSem, 0, 0);
 
 		tasks[i]->primeList = primeList;
 
-		tasks[i]->maxRank = maxRank;
-		tasks[i]->maxValue = maxValue;
 		tasks[i]->progression = &progression;
 		tasks[i]->nbrThreads = &nbrThreads;
-		tasks[i]->initValue = &initValue;
 		tasks[i]->workInProgress = &workInProgress;
-
-		tasks[i]->workerOrchestrator = &workerOrchestrator;
 	}
 
-	// Launches threads
-	for (i = 0; i < nbrMaxThreads; i++) {
-		pthread_create(&tasks[i]->thread, NULL, primeNumbersWorker, tasks[i]);
-	}
+	tasks[0]->initValue = 5;
+	pthread_create(&tasks[0]->thread, NULL, primeNumbersWorker, tasks[0]);
 
 	unsigned int oldNbrThreads;
 	unsigned int primeListIterator = 1;
 	unsigned int tmpElement;
 	unsigned char allTasksCompleted;
+	unsigned char allTasksBuffered;
+
 	while(workInProgress) {
 		oldNbrThreads = nbrThreads;
 
 		allTasksCompleted = 1;
+		allTasksBuffered = 1;
 		for (i = 0; i < oldNbrThreads; i++) {
 			if (!tasks[i]->workCompleted) {
 				allTasksCompleted = 0;
 			}
 
-			if (tasks[i]->primeNumberBuffered) {
+			if (!tasks[i]->primeNumberBuffered) {
+				allTasksBuffered = 0;
+			} else {
 				if (progression >= *maxRank) {
 					workInProgress = 0;
 					break;
@@ -137,9 +130,10 @@ unsigned long long int *primeNumbers(unsigned int *maxRank, unsigned long long i
 				sem_post(&tasks[i]->primeNumberBufferedSem);
 				
 				if (progression % OPERATIONS_BEFORE_NEW_THREAD == 0 && nbrThreads < nbrMaxThreads) {
+					tasks[nbrThreads]->initValue = tasks[i]->primeNumberBuffer + 2;
+					
+					pthread_create(&tasks[nbrThreads]->thread, NULL, primeNumbersWorker, tasks[nbrThreads]);
 					nbrThreads++;
-					initValue = tasks[i]->primeNumberBuffer + 2;
-					sem_post(&workerOrchestrator);
 				}
 			}
 		}
@@ -147,6 +141,7 @@ unsigned long long int *primeNumbers(unsigned int *maxRank, unsigned long long i
 		if (allTasksCompleted) {
 			workInProgress = 0;
 			*maxRank = progression;
+			break;
 		}
 
 		// Bubble sort
@@ -155,6 +150,7 @@ unsigned long long int *primeNumbers(unsigned int *maxRank, unsigned long long i
 			primeList[primeListIterator] = primeList[primeListIterator - 1];
 			primeList[primeListIterator - 1] = tmpElement;
 		}
+
 		if (primeListIterator++ >= progression - 1) {
 			primeListIterator = 1;
 		}
@@ -165,7 +161,6 @@ unsigned long long int *primeNumbers(unsigned int *maxRank, unsigned long long i
 		sem_destroy(&tasks[i]->primeNumberBufferedSem);
 		free(tasks[i]);
 	}
-	sem_destroy(&workerOrchestrator);
 	free(tasks);
 
 	return primeList;
